ab7a1296e10bc0ce18177b17a0531f679bbc515d
prasoonagrawal <prasoonagrawal8@gmail.com>
2021-08-31 13:48:24 +0530

    12CHAPTER

--- bmicalculator/lib/inputpage.dart
+++ bmicalculator/lib/inputpage.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'RoundButtonClass.dart';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -218,25 +219,5 @@ class _InputPageState extends State<InputPage> {
             )
           ],
         ));
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  @override
-  RoundIconButton({required this.icon, required this.onPressed});
-  final IconData icon;
-  final Function()? onPressed;
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 6.0,
-      constraints: BoxConstraints.tightFor(
-        width: 56,
-        height: 56,
-      ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      fillColor: Color(0xFF4C4F5E),
-    );
   }
 }
