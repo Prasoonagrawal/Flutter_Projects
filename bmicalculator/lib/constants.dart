45ae5b956cea88c1504a00b7d3ac5df21afb67dc
prasoonagrawal <prasoonagrawal8@gmail.com>
2021-08-31 16:04:50 +0530

    colour

--- bmicalculator/lib/constants.dart
+++ bmicalculator/lib/constants.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 const kBottomContainerHeight = 80.0;
 const kActiveCardColor = Color(0xFF1D1E33);
 const KInactiveCardColor = Color(0xFF111328);
-const kBottomcontainerColor = Color(0xFFF75C1E);
-const kOverlayColor = Color(0x29F75C1E);
+const kBottomcontainerColor = Color(0xFFF129827); //Color(0xFFF75C1E);
+const kOverlayColor = Color(0x29129827);
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
