be657be8a98a10a363926dcba935e53223acbc10
prasoonagrawal <prasoonagrawal8@gmail.com>
2021-09-06 18:21:16 +0530

    6sep

--- climateapp/lib/screens/loading_screen.dart
+++ climateapp/lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:climateapp/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,8 +23,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=148118c920f5cf579fcbb1d02f63d0e9'));
+    //  print(response.body);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
