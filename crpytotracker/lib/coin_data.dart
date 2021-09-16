3ae0f320f880c13f91a76d4962eba937ea4e0eb5
prasoonagrawal <prasoonagrawal8@gmail.com>
2021-09-16 21:11:25 +0530

    16Sep

--- crpytotracker/lib/coin_data.dart
+++ crpytotracker/lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -27,5 +31,20 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
+const coinapiurl = "http://rest.coinapi.io/v1/exchangerate";
+const apikey = "EA662E7C-379B-4E33-A690-CE3BEDE2BC46";
 
-class CoinData {}
+class CoinData {
+  Future getcoindata() async {
+    String url = '$coinapiurl/BTC/USD/$apikey';
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var decodedata = jsonDecode(response.body);
+      var price = decodedata['rate'];
+      return price;
+    } else {
+      print("URL not found");
+    }
+  }
+}
